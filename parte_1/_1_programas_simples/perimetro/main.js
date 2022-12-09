e38a74369d01446b924aca9d59a661bd8ab5893d
SolYX1349 <saul77023@gmail.com>
2022-12-08 23:47:02 -0600

    Se agrego un ejercicio de perimetro

---
+++ parte_1/_1_programas_simples/perimetro/main.js
@@ -0,0 +1,5 @@
+let radio = 15.4;
+let diametro = radio * 2;
+let perimetro = diametro * 3.1416;
+let area = (3.1416 * radio) * radio;
+console.log("Perimetro = " + perimetro + " Area = " + area + " Diametro = " + diametro);
