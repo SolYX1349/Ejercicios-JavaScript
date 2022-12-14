657016f782ca63c4d360104f9f6d56288657f7e6
SolYX1349 <saul77023@gmail.com>
2022-12-13 20:58:44 -0600

    Se agregan mas ejecicios js

---
+++ parte_1/_3_ciclos/ciclo_con_break/Main.js
@@ -0,0 +1,7 @@
+for (let i = 0; (i = 100); i++) {
+  if (i = 10) {
+    console.log(i);
+    break;
+  }
+}
+console.log("El contador ha finalizado.");
---
+++ parte_1/_3_ciclos/ciclo_while/main.js
@@ -0,0 +1,7 @@
+let contador = 6;
+let numero = 1;
+while (numero < contador) {
+    console.log(numero);
+    numero++;
+}
+console.log("El ciclo ha finalizado.");
