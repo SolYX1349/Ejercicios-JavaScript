c83f8cdef72de5f5e0d57ef439b58da20e22316f
SolYX1349 <saul77023@gmail.com>
2023-01-28 19:36:19 -0600

    Se agregaron dos ejercicios de programas simples

---
+++ parte_1/_1_programas_simples/longitud_nombre/Main.js
@@ -0,0 +1,5 @@
+let nombre = "Alberto";
+
+let longNombre = nombre.length;
+
+console.log("Nombre contiene " + longNombre + " caracteres.");
---
+++ parte_1/_1_programas_simples/recargas/Main.js
@@ -0,0 +1,9 @@
+let numeroTelefonico = 1234567890;
+let valorVerificacion = 1234567890;
+let mensaje;
+if(numeroTelefonico === valorVerificacion){
+    mensaje = "Logro pasar su recarga.";
+}else{
+    mensaje = "Error al verificar su numero es distinto.";
+}
+console.log(mensaje);
