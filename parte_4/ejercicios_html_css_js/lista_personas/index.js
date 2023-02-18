26c15243c22e93541ffc76aa79bdd1dcc7e84ee7
SolYX1349 <saul77023@gmail.com>
2023-02-17 19:19:09 -0600

    Se arreglo un error de validacion en el ultimo ejercicio.

--- parte_4/ejercicios_html_css_js/lista_personas/index.js
+++ parte_4/ejercicios_html_css_js/lista_personas/index.js
@@ -4,16 +4,23 @@ document
 let persona = new Persona();
 function agregarPersona() {
   let elemento = document.forms["formulario"];
-  persona.agregarPersona(elemento["nombre"].value , elemento["apellido"].value);
-  let texto = "";
-  for (let objeto of persona.getArreglo) {
-    texto += `<li>${objeto}</li>`
+  if (elemento["nombre"].value != "" && elemento["apellido"].value != "") {
+    persona.agregarPersona(
+      elemento["nombre"].value,
+      elemento["apellido"].value
+    );
+    let texto = "";
+    for (let objeto of persona.getArreglo) {
+      texto += `<li>${objeto}</li>`;
+    }
+    document.getElementById("lista-desordenada").innerHTML = texto;
+    limpiarInput();
+  }else{
+    alert("Tienes que llenar los campos Nombre y Apellido");
   }
-  document.getElementById("lista-desordenada").innerHTML = texto;
-  limpiarInput();
 }
-function limpiarInput(){
-    let elemento = document.forms["formulario"];
-    elemento["nombre"].value = "";
-    elemento["apellido"].value = "";
+function limpiarInput() {
+  let elemento = document.forms["formulario"];
+  elemento["nombre"].value = "";
+  elemento["apellido"].value = "";
 }
